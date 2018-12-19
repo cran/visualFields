d4507712a6cf283bf4e54ef2abc0490b20fd383a
Ivan Marin-Franch <imarinfr@optocom.es>
2018-12-19 10:30:03 +0000

    version 0.6

--- R/hist_poplr.R
+++ R/hist_poplr.R
@@ -1,32 +1,44 @@
-hist_poplr <- function( scomb_obs, pcomb_obs, scomb, nLoc = 52, txtfont = "sans", pointsize = 10 ) {
+hist_poplr <- function( s, pval, sp, nLoc = 52, showLabels = TRUE,
+                        txtfont = "sans", pointsize = 10,
+                        plt = c( 0, 1.0, 0.3, 1 ), mpg = c( 1.85, 0.5, 0 ),
+                        coltxt  = rgb( red = 1.0, green = 0.0, blue = 0.0 ),
+                        colhist = rgb( red = 1.0, green = 0.0, blue = 0.0, alpha = 0.5 ) ) {
 
-  sep <- 0.1
-  scomb_obs <- scomb_obs / nLoc
-  scomb     <- scomb / nLoc
-  pcomb_txt <- paste0( "(p = ", round( pcomb_obs, 3 ), ")" )
-  if( scomb_obs <= 6 ) {
-    scomb_txt <- paste( "S / n = ", round( scomb_obs, 1 ) )
+  sep <- 6 / 100
+  s <- s / nLoc
+  sp     <- sp / nLoc
+  sp[sp > 6] <- 6 # cap to a maximum S/n value of 6 (p-value with 6 decimal places)
+  pval_txt <- paste0( "(p = ", round( pval, 3 ), ")" )
+  if( s <= 6 ) {
+    sp_txt <- round( s, 1 )
   } else {
-    scomb_obs <- 6
-    scomb_txt <- paste( "S / n > 6" )
+    s <- 6
+    sp_txt <- paste( "> 6" )
   }
   adj <- 0.5
-  if( scomb_obs < 1 ) adj <- 0
-  if( scomb_obs > 5 ) adj <- 1
-  breaks    <- seq( 0, 6, by = sep )
-  ops     <- par()$ps
-  ofamily <- par()$family
-  oplt    <- par()$plt
+  if( s < 2 ) adj <- 0
+  if( s > 4 ) adj <- 1
+  breaks  <- seq( 0, 6, by = sep )
+
   par( ps     = pointsize )
   par( family = txtfont )
-  par( plt    = c( 0, 1, 0.3, 1 ) )
-  hist( scomb, breaks = breaks, freq = FALSE, main = "", xlim = c( 0, 6 ), ylim = c( 0, 1.3 ), border = rgb( 0.7, 0.7, 0.7 ), col = rgb( 0.9, 0.9, 0.9 ), axes = FALSE )
-  axis( 1, las = 1, tcl = -.3, lwd = 0.5, lwd.ticks = 0.5 )
-  title( xlab = "S / number of locations n", mgp = c( 2, 1, 0 ) )
-  lines( c( scomb_obs, scomb_obs ), c( 0, 1.1 ), col = "red" )
-  points( c( scomb_obs ), 1.1, pch = 21, col = "red", bg = "red" )
-  text( c( scomb_obs ), 1.2, labels = paste( scomb_txt, pcomb_txt ), adj = adj )
-  par( ps     = ops )
-  par( family = ofamily )
-  par( plt    = oplt )
+  par( plt = plt )
+  par( mgp = mpg )
+
+  ymax <- max( hist( sp, breaks = breaks, plot = FALSE )$density )
+  xlim <- c( 0, 6 )
+  ylim <- c( 0, ymax )
+  at     <- c( 0, 2, 4, 6 )
+  if( showLabels ) labels <- c( 0, 2, 4, 6 )
+  else             labels <- c( "", "", "", "" )
+  hist( sp, breaks = breaks, freq = FALSE, main = "",
+        xlim = xlim, ylim = ylim,
+        xlab = "", ylab = "", lty = 0,
+        col = colhist,
+        axes = FALSE, ann = FALSE )
+  axis( 1, at = at, labels = labels, tcl = -0.3, lwd = 0.5, lwd.ticks = 0.5 )
+  lines( c( s, s ), 0.8 * c( 0, ymax ), col = coltxt )
+  points( s, 0.8 * ymax, pch = 21, col = coltxt, bg = coltxt, cex = 0.7 )
+  text( s, 0.9 * ymax, labels = paste( sp_txt, pval_txt ), adj = adj, cex = 0.7 )
+  title( xlab = "S / n" )
 }
